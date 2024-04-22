3f309fe58680ba987fbf3cb3ecb357e30eca0b4c
AlexMiller93 <alexandr.miller93@gmail.com>
2024-04-22 20:56:08 +0300

    update admin site

--- foods/admin.py
+++ foods/admin.py
@@ -4,6 +4,14 @@ from foods.models import Food, FoodCategory
 
 # Register your models here.
 
-admin.site.register(Food)
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    fields = ["category", "code", "name_ru", "cost", "is_publish", "additional"]
+    list_display = ["category", "cost", "name_ru"]
+    search_fields = ["category", "name_ru", "cost"]
 
-admin.site.register(FoodCategory)
+@admin.register(FoodCategory)
+class FoodCategoryAdmin(admin.ModelAdmin):
+    fields = ["id", "name_ru", "order_id"]
+    list_display = ["id", "name_ru"]
+    search_fields = ["name_ru"]
